ad8c2671ba597acb21057573e79561b479f20281
CharanB007 <charanrajraj427@gmail.com>
2022-08-16 14:11:23 +0530

    added abc prompt

--- abc.java
+++ abc.java
@@ -2,14 +2,6 @@ class Yt
 {
 public static void main(String[]args)
 {
-int a=6;
-if(a>3)
-{
-System.out.println('a');
-}
-if(a%2==0)
-{
-System.out.println('b');
-}
+system.out.println("karthick");
 }
 }
